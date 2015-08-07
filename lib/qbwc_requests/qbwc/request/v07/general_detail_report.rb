c50fae2d71780fb7e4cc7d535dcdacb789fad2fd
Alexandre Mondaini Calvão <apotema@gmail.com>
2015-08-07 19:15:23 +0200

    General detail report

---
+++ lib/qbwc_requests/qbwc/request/v07/general_detail_report.rb
@@ -0,0 +1,11 @@
+module Qbwc 
+  module Request
+    module V07
+      class GeneralDetailReport < Qbwc::Request::Base
+
+        
+        
+      end
+    end
+  end
+end
---
+++ spec/qbwc/request/v07/general_detail_report_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe Qbwc::Request::V07::GeneralDetailReport do
+  
+  it_behaves_like 'queryable'
+
+end
