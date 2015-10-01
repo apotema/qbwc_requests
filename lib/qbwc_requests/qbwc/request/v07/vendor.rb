c1f58b45eb587277611e344d1883d8e106a83d8b
joniles <jon.iles@bcs.org.uk>
2015-10-01 18:49:59 +0100

    Add vendor address support

--- lib/qbwc_requests/qbwc/request/v07/vendor.rb
+++ lib/qbwc_requests/qbwc/request/v07/vendor.rb
@@ -1,4 +1,4 @@
-module Qbwc 
+module Qbwc
   module Request
     module V07
       class Vendor < Qbwc::Request::Base
@@ -10,15 +10,21 @@ module Qbwc
         field :first_name
         field :middle_name
         field :last_name
+        field :vendor_address
         field :phone
+        field :alt_phone
+        field :fax
         field :email
         field :contact
+        field :alt_contact
         field :name_on_check
+        field :account_number
+        field :notes
         field :is_vendor_eligible_for_1099
         field :open_balance
 
         validates :name, presence: true
-        
+
       end
     end
   end
