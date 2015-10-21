7bf61eed6fb4358ee0164d94bdf7847bf2986444
Alexandre Mondaini Calvão <apotema@gmail.com>
2015-10-21 13:08:45 +1100

    Init of the references validation

---
+++ spec/qbwc/request/ordered_fields_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+class OrderedFieldsImpl 
+  include Qbwc::OrderedFields
+end
+
+# Specs for Qbwc::OrderedFields
+describe OrderedFieldsImpl do
+
+  describe '#has_one' do
+
+  end
+
+end
---
+++ spec/qbwc/request/v07/reference/vendor_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Qbwc::Request::V07::Reference::Vendor do
+
+  it "should validate the presence of the ListID or the full name" do
+
+    subject.list_ID = "list_Id"
+    subject.full_name = "full name"
+    expect(subject.valid?).to be false
+
+  end
+
+end
