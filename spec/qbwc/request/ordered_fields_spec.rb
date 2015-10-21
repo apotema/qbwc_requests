3a659c31cd3e48f19a2d90666e291d575a29f350
Alexandre Mondaini Calvão <apotema@gmail.com>
2015-10-21 13:30:47 +1100

    Specs for Qbwc::OrderedFields

--- spec/qbwc/request/ordered_fields_spec.rb
+++ spec/qbwc/request/ordered_fields_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
-class OrderedFieldsImpl 
-  include Qbwc::OrderedFields
-end
-
 # Specs for Qbwc::OrderedFields
-describe OrderedFieldsImpl do
+describe Qbwc::OrderedFields do
 
-  describe '#has_one' do
+  let(:ordered_fields) {
+    object = Object.new
+    object.class.include(Qbwc::OrderedFields)
+  }
 
+  describe '#field' do
+
+    before { ordered_fields.field(:some_field) }
+
+    it 'should add a field to the object att_accessors' do
+      ordered_fields.some_field = "10"
+      expect(ordered_fields.some_field).to eql "10"
+    end
+
+    it 'should add a field the list object attributes' do
+      expect(ordered_fields.attr_order.first).to eql :some_field
+    end
+    
   end
 
 end
