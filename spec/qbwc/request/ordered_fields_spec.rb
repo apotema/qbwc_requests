1cf2086120edeece092e8a4504349a0a8e18851a
Alexandre Mondaini Calvão <apotema@gmail.com>
2015-10-21 15:48:15 +1100

    Specs for Qbwc::OrderedFields#ordered_fields

--- spec/qbwc/request/ordered_fields_spec.rb
+++ spec/qbwc/request/ordered_fields_spec.rb
@@ -1,26 +1,50 @@
 require 'spec_helper'
 
-# Specs for Qbwc::OrderedFields
 describe Qbwc::OrderedFields do
 
-  let(:ordered_fields) {
-    object = Object.new
-    object.class.include(Qbwc::OrderedFields)
+  class OrderedFieldsImpl
+    include Qbwc::OrderedFields
+  end
+
+  let(:ordered_field) { 
+    OrderedFieldsImpl.new 
   }
 
   describe '#field' do
 
-    before { ordered_fields.field(:some_field) }
+    before { OrderedFieldsImpl.field(:some_field) }
 
     it 'should add a field to the object att_accessors' do
-      ordered_fields.some_field = "10"
-      expect(ordered_fields.some_field).to eql "10"
+      ordered_field.some_field = "10"
+      expect(ordered_field.some_field).to eql "10"
     end
 
     it 'should add a field the list object attributes' do
-      expect(ordered_fields.attr_order.first).to eql :some_field
+      expect(OrderedFieldsImpl.attr_order.first).to eql :some_field
     end
     
+  end
+
+  describe '#ordered_fields' do
+
+    context 'empty fields' do
+
+      it 'should return a hash' do
+        expect(ordered_field.ordered_fields).to be_kind_of(Hash)
+      end
+
+    end
+
+    context 'populated fields' do
+
+      it 'should return a hash with all the fields and its values' do
+        OrderedFieldsImpl.field(:some_field)
+        ordered_field.some_field = "20"
+        expect(ordered_field.ordered_fields).to eql({some_field: "20"})
+      end
+    
+    end
+
   end
 
 end
