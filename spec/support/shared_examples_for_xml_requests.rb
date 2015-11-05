f1603c61f4d60d1427e1778a3d2c7e49f186cbf6
Alexandre Mondaini Calvão <apotema@gmail.com>
2015-11-05 17:33:24 +1100

    Rspec text improvements

--- spec/support/shared_examples_for_xml_requests.rb
+++ spec/support/shared_examples_for_xml_requests.rb
@@ -1,25 +1,21 @@
 RSpec.shared_examples 'queryable' do
-  
-  describe 'query method' do
     
-    let(:xml) do
-      <<-XML
-        <?xml version="1.0" encoding="ISO-8859-1"?>
-        <?qbxml version="7.0"?>
-        <QBXML>
-          <QBXMLMsgsRq onError="stopOnError">
-            <#{described_class.name.demodulize}QueryRq>
-              <MaxReturned>2000</MaxReturned>
-            </#{described_class.name.demodulize}QueryRq>
-          </QBXMLMsgsRq>
-        </QBXML>
-      XML
-    end
+  let(:xml) do
+    <<-XML
+      <?xml version="1.0" encoding="ISO-8859-1"?>
+      <?qbxml version="7.0"?>
+      <QBXML>
+        <QBXMLMsgsRq onError="stopOnError">
+          <#{described_class.name.demodulize}QueryRq>
+            <MaxReturned>2000</MaxReturned>
+          </#{described_class.name.demodulize}QueryRq>
+        </QBXMLMsgsRq>
+      </QBXML>
+    XML
+  end
 
-    it 'should create a query xml' do
-      expect(described_class.query).to be_xml_equal_to xml
-    end
-
+  it "creates a valid #{described_class.name.gsub(/^.*::/, '')}QueryRq xml" do
+    expect(described_class.query).to be_xml_equal_to xml
   end
 
 end
