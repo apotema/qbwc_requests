50533f03c7466ff466b0f9b045588fbb41d61d50
Thomas Carli <thomas@procore.com>
2016-07-11 21:29:14 +0000

    add fields to general detail report class

--- lib/qbwc_requests/general_detail_report/v07/query.rb
+++ lib/qbwc_requests/general_detail_report/v07/query.rb
@@ -3,6 +3,9 @@ module QbwcRequests
     module V07
       class Query < QbwcRequests::Base
         field :max_returned
+        field :general_detail_report_type
+        field :report_entity_filter
+        field :include_column
       end
     end
   end
