bea47616dde45ee53492b9c051a1dbeb655a61d7
Thomas Carli <thomas@procore.com>
2016-11-16 01:17:54 +0000

    add back in query filter

--- lib/qbwc_requests/general_detail_report/v07/query.rb
+++ lib/qbwc_requests/general_detail_report/v07/query.rb
@@ -2,6 +2,7 @@ module QbwcRequests
   module GeneralDetailReport
     module V07
       class Query < QbwcRequests::Base
+        field :max_returned
         field :general_detail_report_type
         field :report_entity_filter
         field :report_account_filter
