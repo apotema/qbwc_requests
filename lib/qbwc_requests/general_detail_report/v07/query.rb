8ffc32136fd90904fa46a95a0ee182ea9be57e75
Thomas Carli <thomas@procore.com>
2016-11-22 17:15:31 +0000

    add back in valid qbxml field

--- lib/qbwc_requests/general_detail_report/v07/query.rb
+++ lib/qbwc_requests/general_detail_report/v07/query.rb
@@ -8,6 +8,7 @@ module QbwcRequests
         field :report_entity_filter
         field :report_account_filter
         field :report_txn_type_filter
+        field :report_modified_date_range_macro
         field :include_column
       end
     end
