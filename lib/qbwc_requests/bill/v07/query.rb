7c02fd5a12d0913afbe8751d7364bc83c61f6eb7
Eric McAnena <eric.mcanena@procore.com>
2019-08-19 11:03:12 -0700

    Add txn id to bill query

--- lib/qbwc_requests/bill/v07/query.rb
+++ lib/qbwc_requests/bill/v07/query.rb
@@ -2,6 +2,7 @@ module QbwcRequests
   module Bill
     module V07
       class Query < QbwcRequests::Base
+        field :txn_id
         field :max_returned
         field :entity_filter
         field :include_line_items
